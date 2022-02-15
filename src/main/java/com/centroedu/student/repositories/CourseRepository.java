2d1f8a35096ce32cada70b106a2f28ce32564d9d
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-15 17:18:45 -0300

    Fix format

--- src/main/java/com/centroedu/student/repositories/CourseRepository.java
+++ src/main/java/com/centroedu/student/repositories/CourseRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.centroedu.student.entities.Course;
 
-public interface CourseRepository extends JpaRepository<Course, Long>{
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
 }
