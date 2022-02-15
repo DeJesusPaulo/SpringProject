6eb6498cbabfe8f9d35f12352db7dfd97b7b4e00
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-15 17:12:26 -0300

    Fix method findByCourse, add parameter Course id

--- src/main/java/com/centroedu/student/repositories/StudentRepository.java
+++ src/main/java/com/centroedu/student/repositories/StudentRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.centroedu.student.entities.Student;
 import com.centroedu.student.entities.Course;
+import com.centroedu.student.entities.Student;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	
-	public List<Student> findByCurso(Course curso);
+	public List<Student> findByCourse(Course id);
+
 }
