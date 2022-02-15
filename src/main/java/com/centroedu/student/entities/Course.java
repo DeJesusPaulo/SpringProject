3fc1df278bf243d0b36eecbe48bfa7059f36ec77
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-15 17:14:16 -0300

    Fix format

--- src/main/java/com/centroedu/student/entities/Course.java
+++ src/main/java/com/centroedu/student/entities/Course.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,14 +21,15 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
 @Table(name = "courses")
 public class Course {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(name = "name",nullable = false)
+
+	@Column(name = "name", nullable = false)
 	private String name;
 	@Column(name = "shift", nullable = false)
 	private String shift;
