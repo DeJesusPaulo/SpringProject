b3c9f94c72572ee0c5ad700beb496193146280a3
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-17 15:21:09 -0300

    Add methods: FindByDni and FindBySurname

--- src/main/java/com/centroedu/student/repositories/StudentRepository.java
+++ src/main/java/com/centroedu/student/repositories/StudentRepository.java
@@ -10,5 +10,9 @@ import com.centroedu.student.entities.Student;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 	
 	public List<Student> findByCourse(Course id);
+	
+	public Student findByDni(int dni);
+	
+	public List<Student> findBySurname(String surname);
 
 }
