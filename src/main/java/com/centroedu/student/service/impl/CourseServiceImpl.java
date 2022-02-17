2c663c1cdb826e9078119cf0b5a99a1f8795db33
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-17 15:22:20 -0300

    Fix warnings

--- src/main/java/com/centroedu/student/service/impl/CourseServiceImpl.java
+++ src/main/java/com/centroedu/student/service/impl/CourseServiceImpl.java
@@ -1,13 +1,11 @@
 package com.centroedu.student.service.impl;
 
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.centroedu.student.entities.Course;
-import com.centroedu.student.entities.Student;
 import com.centroedu.student.repositories.CourseRepository;
 import com.centroedu.student.service.CourseService;
 
