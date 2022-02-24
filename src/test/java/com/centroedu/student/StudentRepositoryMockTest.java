69ad5997f4a3a5ab9563989bff3b667cfe33cbde
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-24 15:44:28 -0300

    Creation of test Class: Student Repository Mock Test

---
+++ src/test/java/com/centroedu/student/StudentRepositoryMockTest.java
@@ -0,0 +1,38 @@
+package com.centroedu.student;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.centroedu.student.entities.Course;
+import com.centroedu.student.entities.Student;
+import com.centroedu.student.repositories.StudentRepository;
+
+@DataJpaTest
+public class StudentRepositoryMockTest {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Test
+	public void whenFindBySurname_thenReturnListStudents() {
+		Student student01 = Student.builder()
+				.id(11L)
+				.name("Fred")
+				.surname("Sancho")
+				.dni(25649782)
+				.email("mortelale@gmail.com")
+				.course(Course.builder().id(2L).build())
+				.build();
+
+		studentRepository.save(student01);
+		
+		List<Student> founds = studentRepository.findAll();
+		
+		Assertions.assertThat(founds.size()).isEqualTo(11);
+		
+	}
+}
