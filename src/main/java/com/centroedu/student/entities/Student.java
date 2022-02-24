900e5a3671361af78913f205885c9771120b7492
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-02-24 15:43:12 -0300

    Fix field "dni". Fix format

--- src/main/java/com/centroedu/student/entities/Student.java
+++ src/main/java/com/centroedu/student/entities/Student.java
@@ -12,7 +12,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -24,64 +23,60 @@ import lombok.Setter;
 
 /**
  * Class Student with his attributes and methods
+ * 
  * @author Paulo De Jesus
  * @version 1.0
  */
 
 @Entity
 @Table(name = "students")
-@Getter @Setter
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Student {
-	
+
 	/**
 	 * Id of the Student
 	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	/**
 	 * Name of the Student
 	 */
 	@Column(name = "name", nullable = false)
 	@NotEmpty(message = "Debe ingresar un nombre")
 	private String name;
-	
+
 	/**
 	 * Surname of the Student
 	 */
 	@Column(name = "surname", nullable = false)
 	@NotEmpty(message = "Debe ingresar un apellido")
 	private String surname;
-	
+
 	/**
-	 * Dni number of the Stundent
+	 * Dni number of the Student
 	 */
 	@Column(name = "dni", nullable = false, unique = true, length = 10)
-	@Size(max = 10, min = 10, message = "Cantidad de cifras incorrecta")
-	@NotEmpty(message = "Debe ingresar el dni")
 	private int dni;
-	
+
 	/**
 	 * E-mail of the Student
 	 */
-	@Column(name = "email",nullable = false, unique = true)
+	@Column(name = "email", nullable = false, unique = true)
 	@Email(message = "Forma incorrecta de email")
 	private String email;
-	
+
 	@Column(name = "status")
 	private String status;
-	
-	
+
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "course_Id")
-	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler"})
+	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	private Course course;
-	
-	
-	
 
 }
