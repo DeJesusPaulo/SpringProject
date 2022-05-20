eaffe93f38a63fa0f6c53373961ffd84906c457a
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-05-20 15:51:09 -0300

    Adding values to table Students

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,7 +15,18 @@ insert into students(id,name,surname,dni,email,status,course_id) values
 (7,'Andres','Ceratti',25154789,'andres@gmail.com','created',4),
 (8,'Diego','Luna',33569124,'diegote@gmail.com','created',3),
 (9,'Martin','Rojas',32456971,'tinchoro@gmail.com','created',4),
-(10,'Alejandro','Dolina',15649523,'alejo@gmail.com','created',1);
+(10,'Alejandro','Dolina',15649523,'alejo@gmail.com','created',1),
+(11,'Luis','Ventura',33140236,'luifa@gmail.com','created',5),
+(12,'Eduardo','Lujan',30789452,'edu@gmail.com','created',2),
+(13,'Jorge','Rodriguez',40203789,'jorgito@gmail.com','created',1),
+(14,'Roman','Massa',38523401,'roman@gmail.com','created',5),
+(15,'Claudio','Gonzalez',33302854,'claug@gmail.com','created',3),
+(16,'Alfredo','Garcia',38569780,'alfre@gmail.com','created',2),
+(17,'Valentin','Paiva',34555903,'vale@gmail.com','created',4),
+(18,'Emiliano','Savin',35419566,'emis@gmail.com','created',3),
+(19,'Martin','Cabrera',37896452,'maca@gmail.com','created',4),
+(20,'Benjamin','Conan',40894369,'benja@gmail.com','created',1):
+
 
 
 
