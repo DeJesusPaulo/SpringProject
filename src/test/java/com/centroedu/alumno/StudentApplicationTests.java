2b2d032ccb44fa06786f9fbb905671c4e7745d57
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-07-13 09:17:54 -0300

    Reformat Test class

--- src/test/java/com/centroedu/alumno/StudentApplicationTests.java
+++ src/test/java/com/centroedu/alumno/StudentApplicationTests.java
@@ -1,22 +1,13 @@
 package com.centroedu.alumno;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
-
-
-
 @SpringBootTest
-public class AlumnoApplicationTests {
-	
+public class StudentApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
 
-
 }
-
-
