61229c9f271d2a82b87309ae7628333921f6af48
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-08-01 10:00:25 -0300

    Add Exception for list of Students not found

---
+++ src/main/java/com/centroedu/student/utils/exceptions/ListStudentsNotFoundException.java
@@ -0,0 +1,9 @@
+package com.centroedu.student.utils.exceptions;
+
+
+public class ListStudentsNotFoundException extends RuntimeException{
+
+    public ListStudentsNotFoundException(String message){
+        super(message);
+    }
+}
