330d28ba099d607bb954cefd340fddebea0cba5f
De Jesus Paulo <paulo.max.dj06@gmail.com>
2022-08-03 16:57:51 -0300

    Add Student Application Tests Class

---
+++ src/test/java/com/centroedu/test/StudentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.centroedu.test;
+
+import com.centroedu.student.StudentApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@SpringBootTest
+public class StudentApplicationTests {
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
