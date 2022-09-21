6ff9512444dd2df539bb021774258e97c2fbbf64
Paulo De Jesus <paulo.max.dj06@gmail.com>
2022-09-21 15:18:06 -0300

    Drop method Find by Course

--- src/main/java/com/centroedu/student/service/impl/StudentServImp.java
+++ src/main/java/com/centroedu/student/service/impl/StudentServImp.java
@@ -9,16 +9,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.centroedu.student.entities.Course;
+
 import com.centroedu.student.entities.Student;
-import com.centroedu.student.repositories.CourseRepository;
 import com.centroedu.student.repositories.StudentRepository;
 import com.centroedu.student.service.StudentService;
 
 
-
 /**
  * Class that implements the Service for Student Entity
+ *
  * @author Paulo De Jesus
  * @version 1.0
  * @see StudentService
@@ -26,121 +25,122 @@ import com.centroedu.student.service.StudentService;
 
 @Service
 public class StudentServImp implements StudentService {
-	
-	@Autowired
-	StudentRepository studentRepository;
-	@Autowired
-	CourseRepository courseRepository;
-	
-	/**
-	 * Method that retrieves a list of all the Students
-	 * @return a list of Students
-	 */
-	@Override
-	public List<Student> listAllStudents(int offSet, int pageSize) {
-		Pageable pageable = PageRequest.of(offSet, pageSize);
-		Page<Student> students = studentRepository.findAll(pageable);
 
-		List<Student> studentList = students.getContent();
-		return studentList;
-	}
-
-	/**
-	 * Method that retrieves a Student search by his Id
-	 * @param id
-	 * @return a Student
-	 */
-	@Override
-	public Student getStudent(Long id) {
-		
-		return studentRepository.findById(id).orElse(null);
-	}
-
-	/**
-	 * Method that creates a Student
-	 * @param student
-	 * @return the created Student
-	 */
-	@Override
-	public Student  createStudent(Student student) throws StudentNotFoundException {
-
-		if (student == null) { return null; }
-			
-		student.setStatus("Student created");
-		
-		return studentRepository.save(student);
-	}
-	
-	/**
-	 * Method that creates a Student
-	 * @param student
-	 * @return the updated Student
-	 */
-	@Override
-	public Student updateStudent(Student student) {
-		
-		Student student1 = getStudent(student.getId());
-		if (student1 == null) { return null; }
-			
-		student1.setName(student.getName());
-		student1.setSurname(student.getSurname());
-		student1.setDni(student.getDni());
-		student1.setEmail(student.getEmail());
-		student1.setCourse(student.getCourse());
-		student1.setStatus("Student updated");
-		
-		return studentRepository.save(student1);
-	}
-
-	/**
-	 * Method that deletes a Student 
-	 * @param id
-	 * @return the confirmation of the deleted Student
-	 */
-	@Override
-	public Student deleteStudent(Long id) {
-		Student student = getStudent(id);
-		if (student == null) { return null; }		
-		
-		student.setStatus("Student deleted");
-		
-		return studentRepository.save(student);
-	}
-	
-	/**
-	 * Method that retrieves a Student or Students search by his surname
-	 * @param surname
-	 * @return a Student or Students
-	 */
-	@Override
-	public List<Student> findBySurname(String surname) {
-		
-		return studentRepository.findBySurname(surname);
-	}
+    @Autowired
+    StudentRepository studentRepository;
 
 
-	/**
-	 * Method that retrieves the list of Students in a Course
-	 * @param id
-	 * @return all the Students in a Course
-	 */
-	@Override
-	public List<Student> findByCourse(Course id) {	
-		
-		return studentRepository.findByCourse(id);
-		
-	}
+    /**
+     * Method that retrieves a list of all the Students
+     *
+     * @return a list of Students
+     */
+    @Override
+    public List<Student> listAllStudents(int offSet, int pageSize) {
+        Pageable pageable = PageRequest.of(offSet, pageSize);
+        Page<Student> students = studentRepository.findAll(pageable);
 
-	/**
-	 * Method that retrieves a Student search by his Dni number
-	 * @param dni
-	 * @return
-	 */
-	@Override
-	public Student findByDni(String dni) {
-				
-		return studentRepository.findByDni(dni);
-	}
+        List<Student> studentList = students.getContent();
+
+        return studentList;
+    }
+
+    /**
+     * Method that retrieves a Student search by his Id
+     *
+     * @param id of the Student
+     * @return a Student search by his Id
+     */
+    @Override
+    public Student getStudent(Long id) {
+
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    /**
+     * Method that creates a Student
+     *
+     * @param student set as a parameter
+     * @return the created Student
+     */
+    @Override
+    public Student createStudent(Student student) throws StudentNotFoundException {
+
+        if (student == null) {
+            return null;
+        }
+
+        student.setStatus("Student created");
+
+        return studentRepository.save(student);
+    }
+
+    /**
+     * Method that creates a Student
+     *
+     * @param student set as a parameter
+     * @return the updated Student
+     */
+    @Override
+    public Student updateStudent(Student student) {
+
+        Student student1 = getStudent(student.getId());
+        if (student1 == null) {
+            return null;
+        }
+
+        student1.setName(student.getName());
+        student1.setSurname(student.getSurname());
+        student1.setDni(student.getDni());
+        student1.setEmail(student.getEmail());
+        student1.setCourse(student.getCourse());
+        student1.setStatus("Student updated");
+
+        return studentRepository.save(student1);
+    }
+
+    /**
+     * Method that deletes a Student
+     *
+     * @param id of the Student
+     * @return the confirmation of the deleted Student
+     */
+    @Override
+    public Student deleteStudent(Long id) {
+        Student student = getStudent(id);
+        if (student == null) {
+            return null;
+        }
+
+        student.setStatus("Student deleted");
+
+        return studentRepository.save(student);
+    }
+
+    /**
+     * Method that retrieves a Student or Students search by his surname
+     *
+     * @param surname of the Student
+     * @return a Student or a List Students
+     */
+    @Override
+    public List<Student> findBySurname(String surname) {
+
+        return studentRepository.findBySurname(surname);
+    }
+
+
+    /**
+     * Method that retrieves a Student search by his Dni number
+     *
+     * @param dni of the Student
+     * @return a Student search by his Dni number
+     */
+    @Override
+    public Student findByDni(String dni) {
+
+        return studentRepository.findByDni(dni);
+    }
 
 }
 	
