e5c2df62630fc6ac213f44e339d776a3e6259d85
Paulo De Jesus <paulo.max.dj06@gmail.com>
2022-09-21 15:43:31 -0300

    Add Exception for a course not found

---
+++ src/main/java/com/centroedu/student/utils/exceptions/CourseNotFoundException.java
@@ -0,0 +1,6 @@
+package com.centroedu.student.utils.exceptions;
+
+public class CourseNotFoundException extends RuntimeException{
+
+        public CourseNotFoundException(String message) { super(message); }
+}
