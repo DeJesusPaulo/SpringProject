1b82940f09f4c1556fcd51a658d4e8386aa44e77
Paulo De Jesus <paulo.max.dj06@gmail.com>
2022-09-21 15:43:48 -0300

    Add Exception for a dni not found

---
+++ src/main/java/com/centroedu/student/utils/exceptions/DniNotFoundException.java
@@ -0,0 +1,6 @@
+package com.centroedu.student.utils.exceptions;
+
+public class DniNotFoundException extends RuntimeException{
+
+    public DniNotFoundException(String message) { super(message); }
+}
