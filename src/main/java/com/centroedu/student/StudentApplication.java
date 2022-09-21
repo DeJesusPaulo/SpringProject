28270a9c81532be13e11b9a5cb00d3353c102261
Paulo De Jesus <paulo.max.dj06@gmail.com>
2022-09-21 15:26:32 -0300

    Add Exclusion of Security Auto Configuration

--- src/main/java/com/centroedu/student/StudentApplication.java
+++ src/main/java/com/centroedu/student/StudentApplication.java
@@ -2,10 +2,10 @@ package com.centroedu.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class StudentApplication {
 
 	public static void main(String[] args) {
